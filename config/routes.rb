dcd82fc5055571376e5d9a6c4deabe48358da4ed
Jameson Knoll <j.knoll621@gmail.com>
2018-01-01 23:50:47 +0800

    basic signup and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints Clearance::Constraints::SignedIn.new do 
+    root to: 'dashboards#show'
+  end
+  root to: "homes#show"
 end
