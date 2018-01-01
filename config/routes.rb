276e7211791f46a8c9c502d3ce34ee544f6009c4
Jameson Knoll <j.knoll621@gmail.com>
2018-01-02 00:52:27 +0800

    user authentication routing and forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
     root to: 'dashboards#show'
   end
   root to: "homes#show"
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "sessions", only: [:create]
+  resources :users, only: [:create, :show] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
 end
