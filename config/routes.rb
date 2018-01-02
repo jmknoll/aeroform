c76eb5614cc471080a53efd94ffedfa8ee0a57cf
Jameson Knoll <j.knoll621@gmail.com>
2018-01-02 12:06:41 +0800

    sign in, sign out, reset password

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about', as: 'about'
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "sessions", only: [:create]
+  resource :session, controller: "clearance/sessions", only: [:create]
   resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
 
 
   get "/sign_up" => "users#new", as: "sign_up"
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
 end
