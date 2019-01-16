f2695d84a33b4e48ec8e8508c89ba36605fa595e
Jameson Knoll <j.knoll621@gmail.com>
2019-01-16 17:27:04 +0800

    refactor api key generation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
   ## This should be refactored in a more sensible way
   def generate_api_key(user=nil)
     user ||= current_user
-    api_key = ApiKey.new
-    api_key.key = ApiKey.generator
-    api_key.user = user
-    api_key.save!
-    @user = current_user
-    render template: "dashboards/show"
+    api_key = ApiKey.new do |a|
+      a.key = ApiKey.generator
+      a.user = user
+    end
+    if api_key.save!
+      render template: "dashboards/show"
+    end
   end
 end
